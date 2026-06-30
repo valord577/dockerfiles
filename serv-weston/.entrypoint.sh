da765d69fd2705549ad12418512eaac53aec08d5
valord577 <valord577@gmail.com>
2026-07-01 00:05:06 +0800

    disable novnc

--- serv-weston/.entrypoint.sh
+++ serv-weston/.entrypoint.sh
@@ -5,12 +5,12 @@ passwd -d root
 
 mkdir -p ${XDG_RUNTIME_DIR}; mkdir -p /tmp/.X11-unix;
 
-cert='/tmp/tls.cert'; key='/tmp/tls.key'
-openssl req -x509 -nodes -days 3660 -newkey rsa:4096 \
-  -keyout ${key} -out ${cert} \
-  -subj "/C=XX/ST=Self/L=Self/O=Self/OU=Self/CN=Self"
-/opt/websockify/run --cert=${cert} --key=${key} \
-  -D --web=/opt/novnc/ 0.0.0.0:55900 127.0.0.1:5900
+#cert='/tmp/tls.cert'; key='/tmp/tls.key'
+#openssl req -x509 -nodes -days 3660 -newkey rsa:4096 \
+#  -keyout ${key} -out ${cert} \
+#  -subj "/C=XX/ST=Self/L=Self/O=Self/OU=Self/CN=Self"
+#/opt/websockify/run --cert=${cert} --key=${key} \
+#  -D --web=/opt/novnc/ 0.0.0.0:55900 127.0.0.1:5900
 
 
 WESTON_COMMAND=$(cat <<- EOF
