3fbbb3a7f603ace8cfc058e39e2fb2b554d75052
valord577 <valord577@gmail.com>
2026-07-01 22:34:26 +0800

    add ndk r27d

--- crossbuild-all-in-one/extract_llvm-mingw.sh
+++ crossbuild-all-in-one/extract_llvm-mingw.sh
@@ -1,27 +1,35 @@
 #!/usr/bin/env sh
 set -e
 
-MINGW_ROOT="/opt/toolchain/llvm-mingw"
+MINGW_ROOT="/opt/toolchain/llvm-mingw"; mkdir -p "${MINGW_ROOT}"
 
-_DOWNLOAD_URL_="${MINGW_URL}/${MINGW_VERSION}/llvm-mingw-${MINGW_VERSION}-${MINGW_DEFAULT}-$(uname -m).tar.xz"
-curl --fail-with-body -sSL -o "1.tar.xz" --url "${_DOWNLOAD_URL_}"
-mkdir -p "${MINGW_ROOT}" && tar -xvf "1.tar.xz" -C "${MINGW_ROOT}" --strip-components=1 --no-same-owner
+filename="llvm-mingw-${MINGW_VERSION}-${MINGW_DEFAULT}-$(uname -m)"
+_DOWNLOAD_URL_="${MINGW_URL}/${MINGW_VERSION}/${filename}.tar.xz"
+curl --fail-with-body -sSL -o "${filename}.tar.xz" --url "${_DOWNLOAD_URL_}"
+
+
+extractor="tar -xvf '${filename}.tar.xz' -C '${MINGW_ROOT}' --strip-components=1 --no-same-owner '${filename}'"
+eval "${extractor}/'generic-w64-mingw32/'"
+eval "${extractor}/'aarch64-w64-mingw32/include'"
+eval "${extractor}/'aarch64-w64-mingw32/lib/'"
+rm -rf -- ${MINGW_ROOT}/aarch64-w64-mingw32/lib/libc++.modules.json
+eval "${extractor}/'x86_64-w64-mingw32/include'"
+eval "${extractor}/'x86_64-w64-mingw32/lib/'"
+rm -rf -- ${MINGW_ROOT}/x86_64-w64-mingw32/lib/libc++.modules.json
+#eval "${extractor}/'lib/clang/19/include/'"
+eval "${extractor}/'lib/clang/19/lib/windows/libclang_rt.builtins-aarch64.a'"
+eval "${extractor}/'lib/clang/19/lib/windows/libclang_rt.builtins-x86_64.a'"
+eval "${extractor}/'bin/'"
 
 # remove unused files...
-rm -rf -- ${MINGW_ROOT}/i686-w64-mingw32
-rm -rf -- ${MINGW_ROOT}/armv7-w64-mingw32
-
-rm -rf -- ${MINGW_ROOT}/share
-rm -rf -- ${MINGW_ROOT}/**/share
-
 rm -rf -- ${MINGW_ROOT}/generic-w64-mingw32/include/**/*.idl
+rm -rf -- ${MINGW_ROOT}/bin/*-widl
 
 rm -rf -- ${MINGW_ROOT}/bin/i686-w64-mingw32*
 rm -rf -- ${MINGW_ROOT}/bin/i686-w64-windows-gnu.cfg
 rm -rf -- ${MINGW_ROOT}/bin/armv7-w64-mingw32*
 rm -rf -- ${MINGW_ROOT}/bin/armv7-w64-windows-gnu.cfg
-rm -rf -- ${MINGW_ROOT}/bin/x86_64-w64-mingw32uwp*
-rm -rf -- ${MINGW_ROOT}/bin/aarch64-w64-mingw32uwp*
+rm -rf -- ${MINGW_ROOT}/bin/*-w64-mingw32uwp*
 rm -rf -- ${MINGW_ROOT}/bin/analyze-build
 rm -rf -- ${MINGW_ROOT}/bin/clangd
 rm -rf -- ${MINGW_ROOT}/bin/clang-format
@@ -31,28 +39,21 @@ rm -rf -- ${MINGW_ROOT}/bin/intercept-build
 rm -rf -- ${MINGW_ROOT}/bin/lldb*
 rm -rf -- ${MINGW_ROOT}/bin/run-clang-tidy
 rm -rf -- ${MINGW_ROOT}/bin/scan-build-py
-rm -rf -- ${MINGW_ROOT}/bin/*-widl
 rm -rf -- ${MINGW_ROOT}/bin/*clang-scan-deps
 rm -rf -- ${MINGW_ROOT}/bin/clang-scan-deps-wrapper
 rm -rf -- ${MINGW_ROOT}/bin/clang-target-wrapper
 
-rm -rf -- ${MINGW_ROOT}/lib/libear
-rm -rf -- ${MINGW_ROOT}/lib/libscanbuild
-rm -rf -- ${MINGW_ROOT}/lib/liblldbIntelFeatures.so*
 
-
-
-rm -rf -- ${MINGW_ROOT}/aarch64-w64-mingw32/bin
-rm -rf -- ${MINGW_ROOT}/aarch64-w64-mingw32/lib/libc++.modules.json
-rm -rf -- ${MINGW_ROOT}/x86_64-w64-mingw32/bin
-rm -rf -- ${MINGW_ROOT}/x86_64-w64-mingw32/lib/libc++.modules.json
-
+rm -rf -- ${MINGW_ROOT}/bin/clang
+rm -rf -- ${MINGW_ROOT}/bin/clang++
 rm -rf -- ${MINGW_ROOT}/bin/clang-19
+rm -rf -- ${MINGW_ROOT}/bin/clang-cpp
 rm -rf -- ${MINGW_ROOT}/bin/ld.lld
 rm -rf -- ${MINGW_ROOT}/bin/lld
 rm -rf -- ${MINGW_ROOT}/bin/lld-link
 rm -rf -- ${MINGW_ROOT}/bin/llvm-addr2line
 rm -rf -- ${MINGW_ROOT}/bin/llvm-ar
+rm -rf -- ${MINGW_ROOT}/bin/llvm-dlltool
 rm -rf -- ${MINGW_ROOT}/bin/llvm-cov
 rm -rf -- ${MINGW_ROOT}/bin/llvm-cvtres
 rm -rf -- ${MINGW_ROOT}/bin/llvm-cxxfilt
@@ -61,6 +62,7 @@ rm -rf -- ${MINGW_ROOT}/bin/llvm-ml
 rm -rf -- ${MINGW_ROOT}/bin/llvm-nm
 rm -rf -- ${MINGW_ROOT}/bin/llvm-objcopy
 rm -rf -- ${MINGW_ROOT}/bin/llvm-objdump
+rm -rf -- ${MINGW_ROOT}/bin/llvm-ranlib
 rm -rf -- ${MINGW_ROOT}/bin/llvm-pdbutil
 rm -rf -- ${MINGW_ROOT}/bin/llvm-profdata
 rm -rf -- ${MINGW_ROOT}/bin/llvm-rc
@@ -68,20 +70,11 @@ rm -rf -- ${MINGW_ROOT}/bin/llvm-readelf
 rm -rf -- ${MINGW_ROOT}/bin/llvm-readobj
 rm -rf -- ${MINGW_ROOT}/bin/llvm-size
 rm -rf -- ${MINGW_ROOT}/bin/llvm-strings
+rm -rf -- ${MINGW_ROOT}/bin/llvm-strip
 rm -rf -- ${MINGW_ROOT}/bin/llvm-symbolizer
 rm -rf -- ${MINGW_ROOT}/bin/llvm-windres
 rm -rf -- ${MINGW_ROOT}/bin/llvm-wrapper
 
-rm -rf -- ${MINGW_ROOT}/lib/libLLVM*
-rm -rf -- ${MINGW_ROOT}/libclang-cpp*
-
-rm -rf -- ${MINGW_ROOT}/lib/clang/*/lib/windows/libclang_rt.asan*
-rm -rf -- ${MINGW_ROOT}/lib/clang/*/lib/windows/libclang_rt.fuzzer*
-rm -rf -- ${MINGW_ROOT}/lib/clang/*/lib/windows/libclang_rt.profile*
-rm -rf -- ${MINGW_ROOT}/lib/clang/*/lib/windows/libclang_rt.stats*
-rm -rf -- ${MINGW_ROOT}/lib/clang/*/lib/windows/libclang_rt.ubsan_standalone*
-rm -rf -- ${MINGW_ROOT}/lib/clang/*/lib/windows/liborc_rt*
-
 
 # ----------------------------
-rm -f -- 1.tar.xz
+rm -f -- ${filename}.tar.xz
---
+++ crossbuild-all-in-one/extract_ndk_r27d.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+set -e
+
+NDK_VERSION="r27d"
+NDK_EXTRACT_SUFFIX=""
+NDK_URL="https://dl.google.com/android/repository"
+
+NDK_ROOT="/opt/toolchain/ndk-${NDK_VERSION}-slim"
+
+curl --fail-with-body -sSL -X GET -o "/opt/ndk.zip" \
+   --url "${NDK_URL}/android-ndk-${NDK_VERSION}-linux${NDK_EXTRACT_SUFFIX}.zip"
+
+
+extractor="unzip -q /opt/ndk.zip -d '/opt' 'android-ndk-${NDK_VERSION}'"
+
+eval "${extractor}/meta/"'*'
+eval "${extractor}/build/cmake/"'*'
+eval "${extractor}/source.properties"
+
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/AndroidVersion.txt"
+
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/bin/"'yasm'
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/bin/"'ld'
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/bin/"'*-clang'
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/bin/"'*-clang++'
+sed -i 's@usr/bin/env bash@usr/bin/env sh@g' \
+   "/opt/android-ndk-${NDK_VERSION}/toolchains/llvm/prebuilt/linux-x86_64/bin/"*-clang*
+
+#eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/lib/"'clang/*/include/*'
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/lib/"'clang/*/lib/linux/libclang_rt.builtins-*-android.a'
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/lib/"'clang/*/lib/linux/*/libatomic.a'
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/lib/"'clang/*/lib/linux/*/libomp.a'
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/lib/"'clang/*/lib/linux/*/libunwind.a'
+#eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/lib/"'clang/*/lib/linux/*/lldb-server'
+
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/"'usr/include/*'
+eval "${extractor}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/"'usr/lib/*'
+
+
+# Do NOT use the library included with the NDK: zlib
+rm -f "/opt/android-ndk-${NDK_VERSION}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/"{zconf.h,zlib.h}
+rm -f "/opt/android-ndk-${NDK_VERSION}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/"*/libz.a
+rm -f "/opt/android-ndk-${NDK_VERSION}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/"*/*/libz.so
+# Do NOT use the library included with the NDK: c++_shared
+rm -f "/opt/android-ndk-${NDK_VERSION}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/"*/libc++_shared.so
+rm -f "/opt/android-ndk-${NDK_VERSION}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/"*/*/libc++.so
+# Do NOT use the library included with the NDK: gnustl_*
+rm -f "/opt/android-ndk-${NDK_VERSION}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/"*/libstdc++.a
+rm -f "/opt/android-ndk-${NDK_VERSION}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/"*/*/libstdc++.so
+
+
+# ----------------------------
+rm -f -- "/opt/ndk.zip"; mv "/opt/android-ndk-${NDK_VERSION}" "${NDK_ROOT}"
